52617f7b855ae250f5615a15a18e0766021b5dce
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2025-07-30 09:56:43 +0530

    Updated application urls and views

--- src/saas_app/views.py
+++ src/saas_app/views.py
@@ -1,27 +1,84 @@
+import pathlib
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
 
-from visits.models import PageVisited
+from django.http import HttpResponse
+
+from visits.models import PageVisit
+
+LOGIN_URL = settings.LOGIN_URL
+
+this_dir = pathlib.Path(__file__).resolve().parent
 
 
-def home_page_view(request, *args, **kwargs):
-    page_context = {
-        "title": "Welcome to My Website",
-        "content": "This is the home page of my website.",
+def home_view(request, *args, **kwargs):
+    if request.user.is_authenticated:
+        print(request.user.first_name)
+    return about_view(request, *args, **kwargs)
+
+
+def about_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100.0) / qs.count()
+    except:
+        percent = 0
+    my_title = "My Page"
+    html_template = "home.html"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "percent": percent,
+        "total_visit_count": qs.count(),
     }
-
-    path = request.path
-    if path:
-        PageVisited.objects.create(url=path)
-    return render(request, "home.html", page_context)
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_template, my_context)
 
 
-def about_page_view(request, *args, **kwargs):
-    page_context = {
-        "title": "About Us",
-        "content": "This page contains information about our website.",
-    }
+def my_old_home_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {"page_title": my_title}
+    html_ = """
+    <!DOCTYPE html>
+<html>
 
-    path = request.path
-    if path:
-        PageVisited.objects.create(url=path)
-    return render(request, "about.html", page_context)
+<body>
+    <h1>{page_title} anything?</h1>
+</body>
+</html>    
+""".format(
+        **my_context
+    )  # page_title=my_title
+    # html_file_path = this_dir / "home.html"
+    # html_ = html_file_path.read_text()
+    return HttpResponse(html_)
+
+
+VALID_CODE = "abc123"
+
+
+def pw_protected_view(request, *args, **kwargs):
+    is_allowed = request.session.get("protected_page_allowed") or 0
+    # print(request.session.get('protected_page_allowed'), type(request.session.get('protected_page_allowed')))
+    if request.method == "POST":
+        user_pw_sent = request.POST.get("code") or None
+        if user_pw_sent == VALID_CODE:
+            is_allowed = 1
+            request.session["protected_page_allowed"] = is_allowed
+    if is_allowed:
+        return render(request, "protected/view.html", {})
+    return render(request, "protected/entry.html", {})
+
+
+@login_required
+def user_only_view(request, *args, **kwargs):
+    # print(request.user.is_staff)
+    return render(request, "protected/user-only.html", {})
+
+
+@staff_member_required(login_url=LOGIN_URL)
+def staff_only_view(request, *args, **kwargs):
+    return render(request, "protected/user-only.html", {})
