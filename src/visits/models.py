385635f81b6dec0dd471a240bc8197a8922b1328
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2025-07-30 09:55:07 +0530

    Created a landing application

--- src/visits/models.py
+++ src/visits/models.py
@@ -1,15 +1,9 @@
 from django.db import models
 
 
-class PageVisited(models.Model):
-    """
-    Model to track pages visited by users.
-    """
-
-    url = models.URLField(
-        max_length=2000, verbose_name="Page URL", blank=True, null=True
-    )
-    timestamp = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name="Visit Timestamp",
-    )
+# Create your models here.
+class PageVisit(models.Model):
+    # db -> table
+    # id -> hidden -> primary key -> autofield -> 1, 2, 3, 4, 5
+    path = models.TextField(blank=True, null=True)  # col
+    timestamp = models.DateTimeField(auto_now_add=True)  # col
